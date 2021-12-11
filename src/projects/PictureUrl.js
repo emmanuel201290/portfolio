daf07e76db648552d1cd27454e1877d681a3ac84
enmanuelmartinez89@yahoo.es <enmanuelmartinez89@yahoo.es>
2021-12-11 14:31:27 -0600

    portfolio app

---
+++ src/projects/PictureUrl.js
@@ -0,0 +1,17 @@
+export const URL = [
+    {
+        title: 'User registration',
+        link:'https://res.cloudinary.com/emmanuel201290/image/upload/c_crop/v1637096910/ADRL/login_m8jgt2.png'
+    },
+    {
+        title: 'Create an act',
+        link:'https://res.cloudinary.com/emmanuel201290/image/upload/v1635455209/96_izeaab.jpg'
+    },
+    {
+        title: 'Show information about act created',
+        link:'https://res.cloudinary.com/emmanuel201290/image/upload/v1635455209/92_oteacw.jpg'
+    },
+    
+]
+
+export default URL;
