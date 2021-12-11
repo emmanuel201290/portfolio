d93fe6facfe14c87ce5523a6aa6ef2fe90fb3207
enmanuelmartinez89@yahoo.es <enmanuelmartinez89@yahoo.es>
2021-12-11 17:29:10 -0600

    portfolio app

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { PortFolioApp } from './PortfolioApp';
-import './styles/styles.scss';
-
-ReactDOM.render(<PortFolioApp />, document.getElementById('root'));
